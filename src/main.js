b714726d2dfb72ed3c27fe1d450f7602530804ca
Nitin-Tundwal <nitin1238kumar@gmail.com>
2025-03-17 16:31:43 +0530

    done with the words animation

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import CustomShaderMaterial from "three-custom-shader-material/vanilla";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -16,17 +17,17 @@ const cameraPosition = 12;
 const fov =
   2 * Math.atan(window.innerHeight / 2 / cameraPosition) * (180 / Math.PI);
 
-const camera = new THREE.PerspectiveCamera(
+const camera1 = new THREE.PerspectiveCamera(
   25,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 
-camera.position.z = cameraPosition;
+camera1.position.z = cameraPosition;
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("canvas"),
+  canvas: document.getElementById("canvas1"),
   alpha: true,
   antialias: true,
 });
@@ -154,7 +155,7 @@ const loadCan = (positionX, positionY, rotationZ, finalTexture) => {
           start: "top 90%",
           end: "top 20%",
           scrub: 2,
-          markers: true,
+          /* markers: true, */
           onLeave: () => {
             gsap.set(model.position, { x: 1.5, y: -0.5 }); // Fix position when leaving flavor section
           },
@@ -201,7 +202,7 @@ const loadCan = (positionX, positionY, rotationZ, finalTexture) => {
           start: "top 90%",
           end: "top 20%",
           scrub: 2,
-          markers: true,
+          /* markers: true, */
           onLeave: () => {
             gsap.set(model.position, { x: 5, y: -0.5 }); // Fix position when leaving flavor section
           },
@@ -235,7 +236,7 @@ const loadCan = (positionX, positionY, rotationZ, finalTexture) => {
           start: "top 90%",
           end: "top 20%",
           scrub: 2,
-          markers: true,
+          /* markers: true, */
           onLeave: () => {
             gsap.set(model.position, { x: 2, y: 1.3 }); // Fix position when leaving flavor section
           },
@@ -275,7 +276,7 @@ const loadCan = (positionX, positionY, rotationZ, finalTexture) => {
           start: "top 90%",
           end: "top 20%",
           scrub: 2,
-          markers: true,
+          /* markers: true, */
           onLeave: () => {
             gsap.set(model.position, { x: 3.7, y: 1.3 }); // Fix position when leaving flavor section
           },
@@ -316,7 +317,7 @@ const loadCan = (positionX, positionY, rotationZ, finalTexture) => {
           start: "top 90%",
           end: "top 20%",
           scrub: 2,
-          markers: true,
+          /* markers: true, */
           onLeave: () => {
             gsap.set(model.position, { x: 2.2, y: -0.2 }); // Fix position when leaving flavor section
             gsap.set(".heroSeactionCanvas", {
@@ -415,18 +416,185 @@ continous();
 
 // Handle window resize
 window.addEventListener("resize", () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
+  camera1.aspect = window.innerWidth / window.innerHeight;
+  camera1.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
+
 // Animation loop
 function animate() {
   requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+  // controls.update();
+  renderer.render(scene, camera1);
 }
 
 animate();
 
+// Second canvas setup for diving can animation
+const scene2 = new THREE.Scene();
+const camera2 = new THREE.PerspectiveCamera(
+  25,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera2.position.z = cameraPosition;
+
+const renderer2 = new THREE.WebGLRenderer({
+  canvas: document.getElementById("canvas2"),
+  alpha: true,
+  // antialias: true,
+  // preserveDrawingBuffer: true,
+});
+renderer2.setSize(window.innerWidth, window.innerHeight);
+renderer2.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer2.toneMapping = THREE.ACESFilmicToneMapping;
+renderer2.toneMappingExposure = 1;
+
+// Add OrbitControls to second scene
+// const controls = new OrbitControls(camera2, renderer2.domElement);
+// controls.enableDamping = true;
+// controls.dampingFactor = 0.05;
+// controls.enableZoom = true;
+
+// Use the same environment map for the second scene
+// scene2.environment = scene.environment;
+
+// Load HDRI Environment Map
+new RGBELoader().load("../public/assets/hdrs/field.hdr", function (texture) {
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene2.environment = texture;
+});
+
+// Load diving can model
+const divingCanLoader = new GLTFLoader();
+const divingCanDracoLoader = new DRACOLoader();
+divingCanDracoLoader.setDecoderPath(
+  "https://www.gstatic.com/draco/v1/decoders/"
+);
+divingCanLoader.setDRACOLoader(divingCanDracoLoader);
+
+// Load the diving can model
+function loadCloudModel() {
+  divingCanLoader.load("../public/assets/cloud_test/scene.gltf", (gltf) => {
+    const cloud = gltf.scene;
+
+    // More random positioning across width and depth
+    const randomX = ((Math.random() - 0.5) * window.innerWidth) / 45;
+    const randomY = Math.random() * -8;
+    const randomZ = Math.random() * 8 - 4;
+    cloud.position.set(randomX, randomY, randomZ);
+
+    // Random rotation
+    cloud.rotation.x = Math.PI + (Math.random() - 0.5) * 0.5;
+    cloud.rotation.y = Math.random() * Math.PI * 2;
+
+    // Random scale between 0.5 and 1.5
+    const randomScale = 0.5 + Math.random();
+    cloud.scale.set(-randomScale, -randomScale, -randomScale);
+
+    scene2.add(cloud);
+
+    // Random animation duration and delay
+    gsap.to(cloud.position, {
+      y: 5,
+      duration: Math.random() * 4 + 2,
+      delay: Math.random() * 2,
+      ease: "power1.inOut",
+      onComplete: () => {
+        scene2.remove(cloud);
+      },
+    });
+
+    // Random rotation during animation
+    gsap.to(cloud.rotation, {
+      y: cloud.rotation.y + Math.PI * (Math.random() * 2 - 1),
+      duration: Math.random() * 4 + 3,
+      ease: "none",
+    });
+  });
+}
+
+function generateInitialClouds(count) {
+  for (let i = 0; i < count; i++) {
+    loadCloudModel();
+  }
+}
+
+function cloudInterval() {
+  setInterval(() => {
+    loadCloudModel();
+  }, 2000);
+}
+
+// generateInitialClouds(12);
+// cloudInterval();
+
+divingCanLoader.load("../public/assets/models/Soda-can.gltf", (gltf) => {
+  const divingCan = gltf.scene;
+  divingCan.position.set(0, 0, 1);
+  divingCan.scale.set(4, 4, 4);
+  divingCan.rotation.y = Math.PI;
+  divingCan.rotation.z = -0.5; // Removed z-axis rotation
+
+  // Apply materials to the can
+  divingCan.traverse((child) => {
+    if (child.isMesh) {
+      if (child.name === "cylinder_1") {
+        child.material = new THREE.MeshPhysicalMaterial({
+          roughness: 0.1,
+          metalness: 0.75,
+          reflectivity: 1,
+          map: flavorTextures.blackCherry,
+        });
+      } else {
+        child.material = metalMaterial;
+      }
+    }
+  });
+
+  // Small floating animation that keeps the can within the div
+  // gsap.to(divingCan.position, {
+  //   y: 0.3,
+  //   yoyo: true,
+  //   repeat: -1,
+  //   duration: 1,
+  //   ease: "none",
+  // });
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".divingCan",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: 2,
+      markers: true,
+    },
+  });
+
+  tl.from(divingCan.position, {
+    y: 2,
+    x: -3,
+    opacity: 0,
+    duration: 3,
+    ease: "power2.inOut",
+  });
+
+  scene2.add(divingCan);
+});
+
+// Handle window resize for the second canvas
+window.addEventListener("resize", () => {
+  camera2.aspect = window.innerWidth / window.innerHeight;
+  camera2.updateProjectionMatrix();
+  renderer2.setSize(window.innerWidth, window.innerHeight);
+});
+
+function animate2() {
+  requestAnimationFrame(animate2);
+  renderer2.render(scene2, camera2);
+}
+animate2();
 const tl = gsap.timeline();
 
 tl.from(".heading1, .heading2", {
@@ -452,7 +620,7 @@ const tl2 = gsap.timeline({
     start: "top 90%",
     end: "top 80%",
     scrub: 2,
-    markers: true,
+    // markers: true,
   },
 });
 
@@ -461,3 +629,84 @@ tl2.to(".heroSeaction", {
   ease: "none",
   duration: 1,
 });
+
+const tl4 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".divingCan",
+    start: "top -15%",
+    end: "bottom 60%",
+    scrub: true,
+    // markers: true,
+  },
+});
+
+// Animate words sequentially as user scrolls
+tl4
+  // First word animation (25% of the scroll)
+  .fromTo(
+    "#dive",
+    {
+      y: 0,
+      x: 0,
+      scale: 1,
+    },
+    {
+      y: -window.innerHeight,
+      x: -window.innerWidth * 0.9,
+      scale: 0.5,
+      ease: "power2.inOut",
+      transformPerspective: 1000,
+    },
+    0
+  )
+  // Second word animation (next 25% of the scroll)
+  .fromTo(
+    "#into",
+    {
+      y: 0,
+      x: 0,
+      scale: 1,
+    },
+    {
+      y: -window.innerHeight,
+      x: -window.innerWidth * 0.9,
+      scale: 0.5,
+      ease: "power2.inOut",
+      transformPerspective: 1000,
+    },
+    0.25
+  )
+  // Third word animation (next 25% of the scroll)
+  .fromTo(
+    "#better",
+    {
+      y: 0,
+      x: 0,
+      scale: 1,
+    },
+    {
+      y: -window.innerHeight,
+      x: -window.innerWidth * 0.7,
+      scale: 0.5,
+      ease: "power2.inOut",
+      transformPerspective: 1000,
+    },
+    0.5
+  )
+  // Fourth word animation (final 25% of the scroll)
+  .fromTo(
+    "#health",
+    {
+      y: 0,
+      x: 0,
+      scale: 1,
+    },
+    {
+      y: -window.innerHeight,
+      x: -window.innerWidth * 0.7,
+      scale: 0.5,
+      ease: "power2.inOut",
+      transformPerspective: 1000,
+    },
+    0.75
+  );
